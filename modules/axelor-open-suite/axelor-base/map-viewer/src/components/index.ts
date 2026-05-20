f6ab35f984117076fef9d23b30d0f79bfcc7dd45
nursultan <nursultsnjuraev95@gmail.com>
2026-05-20 10:57:23 +0600

    add modules

---
+++ modules/axelor-open-suite/axelor-base/map-viewer/src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as MapView } from "./MapView/MapView.tsx";
+export { default as MarkerGroup } from "./MarkerGroup/MarkerGroup.tsx";
+export { default as MarkerPopup } from "./MarkerPopup/MarkerPopup.tsx";
